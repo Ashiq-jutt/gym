bdbbe5c2a11076cad088001034090ad00e447796
Ashiq-jutt <jtt00088@gmail.com>
2023-02-24 00:21:26 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// // import { Box } from "@mui/material";
+// const App = () => {
+//   const arr = [2, 47777, 5, 600, 9999];
+
+//   const arr2 = { a: [2, 47777, 5, 600, 9999], b: [2, 47777, 5, 600, 9999] };
+
+//   // var output = arr.map((d) => d.toString(2));
+//   // var output = arr.filter((d) => d % 2);
+//   // var output = arr.reduce((acc, curr) => {
+//   //   acc = acc * curr;
+//   //   return acc;
+//   // }, 1);
+//   // var output = arr.reduce((acc, curr) => {
+//   //   if (curr > acc) acc = curr;
+//   //   return acc;
+//   // }, 0);
+//   const obj = [
+//     { age: 26 },
+//     { age: 29 },
+//     { age: 26 },
+//     { age: 20 },
+//     { age: 23 },
+//     { age: 26 },
+//   ];
+//   var output = obj.reduce((acc, curr) => {
+//     if (acc[curr.age]) {
+//       acc[curr.age] = ++acc[curr.age];
+//     } else {
+//       acc[curr.age] = 1;
+//     }
+//     return acc;
+//   }, {});
+
+//   console.log(output, "output");
+//   return <div style={{ display: "flex", background: "red" }}>App</div>;
+// };
+
+// export default App;
---
+++ src/test.js
@@ -0,0 +1,3 @@
+const arr = [2, 4, 5, 6, 99];
+var output = arr.map((d) => d.toString(8));
+console.log(output);
